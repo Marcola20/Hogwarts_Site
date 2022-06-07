71c931ad8cdd3402f5f96eb13cacaa814e529cac
Marcos Vinicius <sbmarcos777@gmail.com>
2022-06-07 14:17:40 -0300

    Adição de itens mágicos e funções no js

---
+++ grifinoria.js
@@ -0,0 +1,19 @@
+function visivel(){
+    document.getElementById("capa_invisibilidade").src="../Hogwarts_Site/Grifinoria_Imagens/capa.png";
+}
+
+function invisivel(){
+    document.getElementById("capa_invisibilidade").src="../Hogwarts_Site/Grifinoria_Imagens/capa_invisivel.png";
+}
+
+function revelio(){
+    document.getElementById("mapa_do_maroto").src="../Hogwarts_Site/Grifinoria_Imagens/mapa_revelio.jpg";
+}
+
+function abrir_mapa(){
+    document.getElementById("mapa_do_maroto").src="../Hogwarts_Site/Grifinoria_Imagens/mapa_aberto.jpg";
+}
+
+function fechar_mapa(){
+    document.getElementById("mapa_do_maroto").src="../Hogwarts_Site/Grifinoria_Imagens/mapa_fechado.jpg";
+}
